f8bf7dc04f789c7de43c6ca4391f8058c95520e9
verasveras <lisaveras@gmail.com>
2017-01-24 10:32:38 -0500

    Incorperate nodemon

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ let app = express();
 app.use(express.static('public'));
 
 app.get('/', function(req, res, next){
-	res.sendFile(path.resolve(__dirname + '/public/react/index.html'));
+	res.sendFile(path.resolve(__dirname + '/public/index.html'));
 })
 
 app.listen(process.env.PORT || 3000);
