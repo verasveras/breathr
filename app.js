020ec1161b5cc5e4da287ae9f8811ecf7cb40785
verasveras <lisaveras@gmail.com>
2017-01-31 09:36:27 -0500

    Skeleton for selecting breath ratio

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ let app = express();
 // app.use('/resources', express.static('node_modules/resources/paper/dist'));
 app.use(express.static('public'));
 
-
 app.get('/paperjs', function(req, res, next){
   res.sendFile(path.join(__dirname, 'node_modules', 'paper', 'dist', 'paper-full.min.js'))
 });
--- public/js/index.js
+++ public/js/index.js
@@ -1,31 +1,40 @@
-document.addEventListener("DOMContentLoaded", function(e) {
+let breath = {
+	in: 0,
+	out: 0
+};
 
-	function changeSelected(event){
+document.addEventListener('DOMContentLoaded', function(e) {
 
-		let target = event.target;
-		let currentTarget = event.currentTarget;
-		console.log(target);
-		target.setAttribute('class', 'selected');
+	function changeSelected(target){
+
+		target.classList.add('selected');
 	}
 
-	function clearAll(){
+	function clearAll(className){
 
-		// let inputsList = document.getElementsByClassName("input");
-		// for (var i = 0; i < inputsList.length; i++){
-		// 	inputsList[i].setAttribute('class', 'unselected');
-		// }
+		let inputsList = document.getElementsByClassName(className);
+		for (var i = 0; i < inputsList.length; i++){
+			inputsList[i].classList.remove('selected');
+		}
 	}
 
 	function handleChange(event){
-		clearAll();
-		changeSelected(event);
+
+		let target = event.target;
+		let className = target.classList.contains('in') ? 'in' : 'out';
+		breath[className] = Number(target.innerHTML);
+		clearAll(className);
+		changeSelected(target);
 
 	}
 
-	let inputsList = document.getElementsByClassName("input");
+	// bind click handlers
+	let inList = document.getElementsByClassName('in');
+	let outList = document.getElementsByClassName('out');
 
-	for (var i = 0; i < inputsList.length; i++){
-		inputsList[i].addEventListener('click', changeSelected);
+	for (var i = 0; i < inList.length; i++){
+		inList[i].addEventListener('click', handleChange);
+		outList[i].addEventListener('click', handleChange);
 	}
 
 })
